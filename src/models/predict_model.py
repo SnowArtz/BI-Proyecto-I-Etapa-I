e8b695c65039c5392192f77af455149886c0a025
SnowArtz <ds.ortiz@uniandes.edu.co>
2023-10-14 02:51:12 -0500

    Prueba re loca papi

--- src/models/predict_model.py
+++ src/models/predict_model.py
@@ -1,12 +1,14 @@
 import pandas as pd
-import time
+import numpy as np
+
 from train_model import Model
 from src.features.BOW import BOW
 from src.features.TFIDF import TFIDF
 
-from sklearn.naive_bayes import MultinomialNB
+#from sklearn.naive_bayes import MultinomialNB
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
 
 
 X_train = pd.read_csv("data/processed/X_train_processed.csv").text
@@ -20,22 +22,74 @@ BOW.fit_transform(X_train, X_test)
 TFIDF = TFIDF()
 TFIDF.fit_transform(X_train, X_test)
 
+print("\nLogistic Regression BOW: ")
+LogisticRegression_BOW = Model(LogisticRegression(max_iter=1000, random_state=42), BOW.X_bow_train, y_train)
+LogisticRegression_BOW.model_grid_search(param_grid = {
+    'penalty': ['l1', 'l2', 'elasticnet'],
+    'C': [0.001, 0.01, 0.1, 1, 10, 100],
+    'solver': ['newton-cg', 'lbfgs', 'liblinear', 'sag', 'saga'],
+    'class_weight': [None, 'balanced'],
+    'fit_intercept': [True, False],
+    'multi_class': ['auto', 'ovr', 'multinomial'],
+    'warm_start': [True, False],
+    'l1_ratio': [None, 0, 0.25,0.5,0.75,1]
+})
+
+
+print("\nLogistic Regression TFIDF: ")
+LogisticRegression_TFIDF = Model(LogisticRegression(max_iter=1000, random_state=42), TFIDF.X_tfidf_train, y_train)
+LogisticRegression_TFIDF.model_grid_search(param_grid = {
+    'penalty': ['l1', 'l2', 'elasticnet'],
+    'C': [0.001, 0.01, 0.1, 1, 10, 100],
+    'solver': ['newton-cg', 'lbfgs', 'liblinear', 'sag', 'saga'],
+    'fit_intercept': [True, False],
+    'multi_class': ['auto', 'ovr', 'multinomial'],
+    'warm_start': [True, False],
+    'l1_ratio': [None, 0, 0.25,0.5,0.75,1]
+})
+
+print("\nRandom Forest BOW: ")
+RandomForest_BOW = Model(RandomForestClassifier(random_state=42), BOW.X_bow_train, y_train)
+RandomForest_BOW.model_grid_search(param_grid = {
+    'n_estimators': [10, 50, 100, 200],
+    'max_depth': [None, 10, 20, 30],
+    'min_samples_split': [2, 5, 10],
+    'min_samples_leaf': [1, 2, 4],
+    'bootstrap': [True, False],
+    'max_features': ['auto', 'sqrt', 'log2'],
+    'criterion': ['gini', 'entropy'],
+})
 
 print("\nRandom Forest TFIDF: ")
 RandomForest_TFIDF = Model(RandomForestClassifier(random_state=42), TFIDF.X_tfidf_train, y_train)
-start_time = time.time()
-RandomForest_TFIDF.model_grid_search(param_grid = {
+RandomForest_TFIDF.model_cross_validate(param_grid = {
     'n_estimators': [10, 50, 100, 200],
     'max_depth': [None, 10, 20, 30],
-    #'min_samples_split': [2, 5, 10],
-    #'min_samples_leaf': [1, 2, 4],
-    #'bootstrap': [True, False],
-    #'max_features': ['auto', 'sqrt', 'log2'],
-    #'criterion': ['gini', 'entropy'],
-    #'class_weight': [None, 'balanced', 'balanced_subsample']
+    'min_samples_split': [2, 5, 10],
+    'min_samples_leaf': [1, 2, 4],
+    'bootstrap': [True, False],
+    'max_features': ['auto', 'sqrt', 'log2'],
+    'criterion': ['gini', 'entropy'],
 })
 
-# Registrar el tiempo de finalización y calcular la duración total
-end_time = time.time()
-duration = end_time - start_time
-print(f"Tiempo de ejecución del programa: {duration:.2f} segundos.")
+print("\nSVM BOW: ")
+SVM_BOW = Model(SVC(random_state=42), BOW.X_bow_train, y_train)
+SVM_BOW.model_grid_search({
+    'C': [0.001, 0.01, 0.1, 1, 10, 100],
+    'kernel': ['linear', 'poly', 'rbf', 'sigmoid'],
+    'gamma': ['scale', 'auto', 0.01, 0.1, 1, 10, 100],
+    'coef0': [0, 1, 2, 3, 4],
+    'shrinking': [True, False],
+    'tol': [1e-3, 1e-4, 1e-5],
+})
+
+print("\nSVM TFIDF: ")
+SVM_TFIDF = Model(SVC(random_state=42), TFIDF.X_tfidf_train, y_train)
+SVM_TFIDF.model_grid_search({
+    'C': [0.001, 0.01, 0.1, 1, 10, 100],
+    'kernel': ['linear', 'poly', 'rbf', 'sigmoid'],
+    'gamma': ['scale', 'auto', 0.01, 0.1, 1, 10, 100],
+    'coef0': [0, 1, 2, 3, 4],
+    'shrinking': [True, False],
+    'tol': [1e-3, 1e-4, 1e-5],
+})
